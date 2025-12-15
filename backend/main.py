b5de33d4c63d764cdae2d5af188c79e225c18d02
bengaudry <bengaudry@outlook.fr>
2025-12-15 14:39:30 +0100

    backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from markupsafe import escape
 from flask import request
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from random import randint
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
@@ -12,6 +15,20 @@ def index():
 
 def home():
     return "Hello, Flask !"
+
+
+@app.route("/search")
+def search():
+    query = request.args.get('q', '')
+    resp = make_response(jsonify({
+        "results": [
+            { "id": "music-1", "name": "Music 1", "artist": "Artist 1", "compatibility": randint(0, 100)/100 },
+            { "id": "music-2", "name": "Music 2", "artist": "Artist 2", "compatibility": randint(0, 100)/100 },
+            { "id": "music-3", "name": "Music 3", "artist": "Artist 3", "compatibility": randint(0, 100)/100 }
+        ],
+    }))
+    resp.headers["Access-Control-Allow-Origin"] = "*"
+    return resp
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -41,29 +58,29 @@ def users_api():
     users = get_all_users()
     return [user.to_json() for user in users]
 '''
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
 
 ###### Appel d API ######
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/call_api")
-def call_api():
-    response = request.get("https://api.github.com")
-    data = response.json()
-    return jsonify(data)
+# @app.route("/call_api")
+# def call_api():
+#     response = request.get("https://api.github.com")
+#     data = response.json()
+#     return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
